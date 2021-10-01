7b8fe82f5a405e29dce398864effdb5cd4b81d9a
EvenZHAng <ezyoung@live.com>
2021-10-01 22:05:19 +0800

    notification with https test

--- project/extra/996ICU/calloffduty.js
+++ project/extra/996ICU/calloffduty.js
@@ -14,16 +14,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function sentNotification() {
     if (Notification.permission !== 'granted') {
-        Notification.requestPermission();
+        Notification.requestPermission().then(function(result) {
+            // result可能是是granted, denied, 或default.
+            console.log(result)
+          });;
         console.log("not granted")
     }
     else {
         var notification = new Notification('Notification title', {
-            icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-            body: 'Hey there! You\'ve been notified!',
+            icon: 'https://evenzhanglll.github.io/project/extra/996ICU/ExitSign.png',
+            body: 'Get off work! Go home!',
         });
         notification.onclick = function () {
-            window.open('http://stackoverflow.com/a/13328397/1269037');
+            window.open('https://evenzhanglll.github.io/project/extra/996ICU/Index.html');
         };
     }
 }
